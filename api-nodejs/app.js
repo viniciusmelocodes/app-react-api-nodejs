c06d5e9a6f660b18b3d91bee78a11ce58e53175a
vinícius lopes de melo <viniciusldemelo@gmail.com>
2021-08-01 16:20:13 -0300

    Correções na renderização

--- api-nodejs/app.js
+++ api-nodejs/app.js
@@ -1,57 +1,59 @@
 /* Incluir a dependência express no prjeto */
-const express = require('express');
+const express = require('express')
 
 /* Iniciar a função do express */
-const app = express();
+const app = express()
 
-var Firebird = require('node-firebird');
+/* Firebird */
+var Firebird = require('node-firebird')
 
-var options = {};
+/* Firebird - opções para conexão */
+var options = {}
 
-options.host = '127.0.0.1';
-options.port = 3050;
-options.database = 'C:\\Users\\development\\Documents\\dev\\TESTE.FDB';
-options.user = 'SYSDBA';
-options.password = 'masterkey';
-options.lowercase_keys = false; // set to true to lowercase keys
-options.role = null;            // default
-options.pageSize = 4096;        // default when creating database
+options.host = '127.0.0.1'
+options.port = 3050
+options.database = 'C:\\Users\\development\\Documents\\dev\\TESTE.FDB'
+options.user = 'SYSDBA'
+options.password = 'masterkey'
+options.lowercase_keys = false // set to true to lowercase keys
+options.role = null            // default
+options.pageSize = 4096        // default when creating database
 
 /* Permitir a manipulação de dados em formato JSON */
-app.use(express.json());
+app.use(express.json())
 
 /* Rota raiz */
 app.get("/", (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.send({ "msg": "Servidor ativo" })
-});
+    res.set('Access-Control-Allow-Origin', '*')
+    return res.json([{ "msg": "Servidor ativo" }])
+})
 
 /* Rota teste */
 app.get("/teste", (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.send({ "msg": "Teste" })
-});
+    res.set('Access-Control-Allow-Origin', '*')
+    return res.json([{ "msg": "Teste" }])
+})
 
 /* Rota dados */
 app.get("/dados", (req, res) => {
     /* CORS */
-    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Origin', '*')
 
     Firebird.attach(options, function (err, db) {
         if (err)
-            throw err;
+            throw err
 
         // db = DATABASE
-        db.query('SELECT FIRST 10 * FROM TABELA_DADOS', function (err, result) {
+        db.query('SELECT FIRST 10 * FROM E_LOGS', function (err, result) {
             // IMPORTANT: close the connection
-            db.detach();
+            db.detach()
 
-            return res.json(result);
-        });
-    });
-});
+            return res.json(result)
+        })
+    })
+})
 
 /* Rodar o projeto na porta 8080 */
 app.listen(8080, () => {
-    console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
-});
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080/")
+})
--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,22 +1,25 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-const uri = 'http://localhost:8080/';
-const query = 'dados';
-
-var dados = [];
+const uri = 'http://localhost:8080/'
+const query = 'teste'
 
 function App() {
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [items, setItems] = useState([])
+
   const chamarAPI = async () => {
     try {
-      const response = await fetch(uri + query, { mode: 'cors' });
-      const data = await response.json();
-      dados = data
-      console.log({ dados })
+      const response = await fetch(uri + query, { mode: 'cors' })
+      const data = await response.json()
+      setIsLoaded(true)
+      setItems(data)
     }
-    catch (e) {
-      console.log(e)
+    catch (error) {
+      setIsLoaded(true)
+      setError(error)
     }
   }
 
@@ -24,24 +27,54 @@ function App() {
     chamarAPI()
   }, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    if (query === 'dados') {
+      var lista =
+        <ul>
+          {items.map(item => (
+            <li key={item.LOGS_ID}>
+              {item.LOGS_NOME} {item.LOGS_DESCRICAO}
+            </li>
+          ))}
+        </ul>
+    } else {
+      var lista =
+        <ul>
+          {items.map(item => (
+            <li key={item.msg}>
+              {item.msg}
+            </li>
+          ))}
+        </ul>
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            Dados API:
+            {lista}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
